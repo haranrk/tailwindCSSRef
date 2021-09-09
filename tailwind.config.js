eca168f4573768c10a5ac9f9d0945f07e5bbbd11
Haran Rajkumar <haranrajkumar97@gmail.com>
2021-09-09 13:44:41 +0530

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  // mode: "jit",
+  purge: ["./public/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        mycolor: colors.red,
+        mygreen: {
+          light: "#ff7ce5",
+          DEFAULT: "#ff49db",
+          dark: "#ff16d1",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
